3203bc334af7968600166e013b23b4fc250fb373
Alumno Ulsa <alumno@ulsa.ulsa-noroeste.edu.mx>
2019-08-19 12:31:38 -0700

    Agregar archivos de proyecto.

---
+++ ConsoleApp1/Alumno.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Alumno
+    {
+        private const string V = "";
+
+        public string Nombre { get; set; }
+        public string Matricula { get; set; }
+        public DateTime FechaDeNacimiento { get; set; }
+        public string Correo { get; set; }
+        public string Direccion { get; set; }
+
+        public Alumno()
+        {
+            Nombre = "Pendiente";
+            Matricula = "Sin asignar";
+  
+        }
+
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Alumno alumno1 = new Alumno();
+            alumno1.Nombre = "Rominice";
+            alumno1.Matricula = "100718";
+
+            Console.WriteLine("Nombre: " + alumno1.Nombre);
+            Console.WriteLine("Matricula: " + alumno1.Matricula);
+            Console.ReadLine();
+        }
+    }
+}
